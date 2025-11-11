fd1f1df5af93a9f3199b9e371d177a08dbdfa257
Esther83-afk <esther.m.chacko-1@ou.edu>
2025-11-10 18:10:19 -0600

    implemented the add method

--- LinkedList.java
+++ LinkedList.java
@@ -2,6 +2,7 @@ public class LinkedList <T> {
 	
 	private Node <T> head;
 	private int size;
+	private Node <T> tail;
 	
 	public static class Node<T> {
 		T data;
@@ -23,8 +24,41 @@ public class LinkedList <T> {
 	}
 	
 	public void add(T element) {
-		
-	}
+		        Node<T> newNode = new Node<>(element);
+		        if (head == null) {
+		            head = tail = newNode;
+		        } else {
+		            tail.next = newNode;
+		            tail = newNode;
+		        }
+		        size++;
+		    }
+
+		    // Adds element at a given index
+		    public void add(int index, T element) {
+		        if (index < 0 || index > size) {
+		            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+		        }
+
+		        Node<T> newNode = new Node<>(element);
+
+		        if (index == 0) {
+		            newNode.next = head;
+		            head = newNode;
+		            if (tail == null) tail = newNode; // first element
+		        } else if (index == size) {
+		            tail.next = newNode;
+		            tail = newNode;
+		        } else {
+		            Node<T> prev = head;
+		            for (int i = 0; i < index - 1; i++) {
+		                prev = prev.next;
+		            }
+		            newNode.next = prev.next;
+		            prev.next = newNode;
+		        }
+		        size++;
+		    }
 	
 	// get() method
 	public T get(int index) {
