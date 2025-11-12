0c52bd3dc7d6221f8670fe390c5b028aa00a0df0
KadenBahner <kaden.m.bahner-1@ou.edu>
2025-11-11 21:00:54 -0600

    Refactor ArrayList class with improved resize method

--- ArrayList.java
+++ ArrayList.java
@@ -71,7 +71,7 @@ public T remove(int index) {
     return removedElement;
 } 
 //helper method to resize underlying array
-public T resize(int newCapacity) {
+public void resize(int newCapacity) {
     T[] newData = (T[]) new Object[newCapacity];
     for (int i = 0; i < size; i++) {
         newData[i] = data[i];
@@ -80,6 +80,7 @@ public T resize(int newCapacity) {
 }
 
 }
+
 
 
 
