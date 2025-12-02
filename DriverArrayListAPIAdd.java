468536ecacc90655196816c65d66bf5a3c50d6c7
Esther83-afk <esther.m.chacko-1@ou.edu>
2025-12-01 23:25:42 -0600

    uploaded Insertion test code

---
+++ DriverArrayListAPIAdd.java
@@ -0,0 +1,187 @@
+package finalCSProject;
+
+import java.util.ArrayList;
+
+public class DriverArrayListAPIAdd {
+
+    ArrayList<Integer> listInt = new ArrayList<>();
+    ArrayList<Double> listDouble = new ArrayList<>();
+        
+    int numberOfRuns;
+    int sizeOfList;
+    int numberOfInsertions;
+    long start;
+    long total = 0;
+    long addTotal = 0;
+    long totalAvg;
+    int index;
+
+    public void loadList(String listType){
+        if (listType=="Integer"){
+            for (int i = 0; i < sizeOfList; i++) {
+                listInt.add(i); //Integer
+            }
+        }
+        else if (listType=="Double"){
+            for (double i = 0.0; i < sizeOfList; i++) {
+                listDouble.add(i); //double
+            }
+        }
+        else {System.out.println("List type needs to be Integer or Double");}
+    }
+   
+    public void runIntegerInsertionTest(String insertionLocation,int sizeOfList){
+        this.sizeOfList=sizeOfList;
+        for (int r = 0; r < numberOfRuns; r++) {
+            loadList("Integer");
+            // TEST 1: Insert at Start (Average)
+            if(insertionLocation=="Start"){
+                index = 0;            
+            } else if(insertionLocation=="Middle"){
+                index=listInt.size()/2;
+            } else{
+                index=listInt.size();
+            }
+            start = System.nanoTime();
+            for (int i = 0; i < numberOfInsertions; i++) {
+                listInt.add(index+i, i);
+            }
+            total = (System.nanoTime() - start);
+            addTotal=total+addTotal;
+            System.out.println("Insertion Integers at " + insertionLocation + " of ArrayListAPI with size of " + sizeOfList + " Run " + r + " Total Time: " + total + " ns");
+
+            listInt.clear(); //clear for next time test
+        }
+        totalAvg = addTotal / numberOfRuns;
+        System.out.println("Total " + numberOfRuns + " runs with Total Avg time: " + totalAvg + " ns");
+        addTotal=0;  //set total back to 0
+    }
+    public void runDoubleInsertionTest(String insertionLocation, int sizeOfList){
+        this.sizeOfList=sizeOfList;
+        for (int r = 0; r < numberOfRuns; r++) {
+            loadList("Double");
+            // TEST 1: Insert at Start (Average)
+            if(insertionLocation=="Start"){
+                index = 0;            
+            } else if(insertionLocation=="Middle"){
+                index=listDouble.size()/2;
+            } else{
+                index=listDouble.size();
+            }
+            start = System.nanoTime();
+            for (double i = 0.0; i < numberOfInsertions; i++) {
+                listDouble.add(index+(int)i, i);
+            }
+            total = (System.nanoTime() - start);
+            addTotal=total+addTotal;
+            System.out.println("Insertion of Doubles at " + insertionLocation + " of ArrayListAPI with size of " + sizeOfList + " Run " + r + " Total Time: " + total + " ns");
+
+            listDouble.clear(); //clear for next time test
+        }
+        totalAvg = addTotal / numberOfRuns;
+        System.out.println("Total " + numberOfRuns + " runs with Total Avg time: " + totalAvg + " ns");
+        addTotal=0;  //set total back to 0
+    }	
+    public static void main(String[] args) {
+        //Test Insertions for List sizes of 10_000, 50_000, 100_000, 300_000, 500_000
+    	//Run Start of list Insertions
+        //10000
+        DriverArrayListAPIAdd testIntList = new DriverArrayListAPIAdd();
+        testIntList.numberOfRuns=10;
+        testIntList.numberOfInsertions=1000;
+        testIntList.runIntegerInsertionTest("Start",10000);       
+
+        //50000
+        testIntList.runIntegerInsertionTest("Start", 50000);        
+
+        //100000
+        testIntList.runIntegerInsertionTest("Start", 100000);  
+
+        //300000
+        testIntList.runIntegerInsertionTest("Start", 300000);  
+
+        //500000
+        testIntList.runIntegerInsertionTest("Start",500000);  
+
+        //Run Double DataType insertion test
+        DriverArrayListAPIAdd testDoubleList = new DriverArrayListAPIAdd();
+        testDoubleList.numberOfRuns=10;
+        testDoubleList.numberOfInsertions=1000;
+        testDoubleList.runDoubleInsertionTest("Start", 10000);
+    	      
+
+        //50000
+        testDoubleList.runDoubleInsertionTest("Start",50000);        
+
+        //100000
+        //testDoubleList.runDoubleInsertionTest("Start",100000);  
+
+        //300000
+        //testDoubleList.runDoubleInsertionTest("Start",300000);  
+
+        //500000
+        //testDoubleList.runDoubleInsertionTest("Start",500000);  
+
+        //Run Middle Insertion
+        testIntList.runIntegerInsertionTest("Middle",10000);
+    	       
+
+        //50000
+        testIntList.runIntegerInsertionTest("Middle",50000);        
+
+        //100000
+        //testIntList.runIntegerInsertionTest("Middle",100000);  
+
+        //300000
+        //testIntList.runIntegerInsertionTest("Middle",300000);  
+
+        //500000
+        //testIntList.runIntegerInsertionTest("Middle",500000);  
+
+        //Run Middle Insertion Double
+        testDoubleList.runDoubleInsertionTest("Middle",10000);       
+
+        //50000
+        testDoubleList.runDoubleInsertionTest("Middle",50000);        
+
+        //100000
+        testDoubleList.runDoubleInsertionTest("Middle",100000);  
+
+        //300000
+        testDoubleList.runDoubleInsertionTest("Middle",300000);  
+
+        //500000
+        testDoubleList.runDoubleInsertionTest("Middle",500000); 
+
+        //Run End Insertion
+        testIntList.runIntegerInsertionTest("End",10000);
+
+        //50000
+        testIntList.runIntegerInsertionTest("End", 50000);        
+
+        //100000
+        testIntList.runIntegerInsertionTest("End",10000);  
+
+        //300000
+        testIntList.runIntegerInsertionTest("End",300000);  
+
+        //500000
+        testIntList.runIntegerInsertionTest("End",500000);  
+
+        //Run Middle Insertion Double
+        testDoubleList.runDoubleInsertionTest("End",10000);      
+
+        //50000
+        testDoubleList.runDoubleInsertionTest("End",50000);        
+
+        //100000
+        testDoubleList.runDoubleInsertionTest("End",100000);  
+
+        //300000
+        testDoubleList.runDoubleInsertionTest("End",300000);  
+
+        //500000
+        testDoubleList.runDoubleInsertionTest("End",500000); 
+    }
+
+}
